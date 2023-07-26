1132b8536b6a812767bbc28dcfe687bf8604da36
andreistaravoitau <andrei.v.staravoitau@gmail.com>
2023-07-26 14:43:32 +0200

    add

---
+++ scripts/listeners.js
@@ -0,0 +1,35 @@
+function displayNone(forms) {
+  for (i = 0; i < forms.length; i++) {
+    forms[i].style.display = "none";
+  }
+}
+
+function onCardClickListener(element) {
+  let forms = document.getElementsByClassName("forms");
+  if (document.getElementById("form-" + element.id).style.display == "block") {
+    displayNone(forms);
+  } else {
+    displayNone(forms);
+    document.getElementById("form-" + element.id).style.display = "block";
+  }
+}
+
+function onBodenChangeListener(event) {
+  var dropdown = event.target;
+  var wandInput = dropdown.closest('.row').querySelector('.form-group input[name="wand"]');
+
+  var dropdownValue = dropdown.value;
+  var newValue = (parseInt(dropdownValue) - 2) + " mm";
+  wandInput.value = newValue;
+}
+
+function onVolumeChange(){
+  let length = parseInt(document.getElementById("laenge").value);
+  let height = parseInt(document.getElementById("hoehe").value);
+
+  document.getElementById("groesse").value = Math.round(length*height*2.3/1000000)
+}
+
+// document.querySelector("#RAL").addEventListener("input", ()=>{
+//
+// })
