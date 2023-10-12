06a44932d28688a7722c1d53a2ead4de4e86d6b8
peterdapanda <peter_nan@protonmail.com>
2023-10-12 15:58:05 +0200

    accordion fix

--- scripts/listeners.js
+++ scripts/listeners.js
@@ -16,27 +16,30 @@ function onCardClickListener(element) {
 
 function onBodenChangeListener(event) {
   var dropdown = event.target;
-  var wandInput = dropdown.closest('.row').querySelector('.form-group input[name="wand"]');
+  var wandInput = dropdown
+    .closest(".row")
+    .querySelector('.form-group input[name="wand"]');
 
   var dropdownValue = dropdown.value;
-  var newValue = (parseInt(dropdownValue) - 2) + " mm";
+  var newValue = parseInt(dropdownValue) - 2 + " mm";
   wandInput.value = newValue;
 }
 
-function onVolumeChange(){
+function onVolumeChange() {
   let length = parseInt(document.getElementById("laenge").value);
   let height = parseInt(document.getElementById("hoehe").value);
 
-  document.getElementById("groesse").value = Math.round(length*height*2.3/1000000)
+  document.getElementById("groesse").value = Math.round(
+    (length * height * 2.3) / 1000000
+  );
 }
 
 // let prePaymentModal = document.querySelector('.prepayment-modal')
 
 // prePaymentModal.addEventListener('click', ()=>{
-  
+
 // })
 
 // document.querySelector("#RAL").addEventListener("input", ()=>{
 //
 // })
-
